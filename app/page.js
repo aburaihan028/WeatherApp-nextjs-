08f0d1271710367560b0a2c8b27eca19e2f1ec9c
MD Abu Raihan <mdaburaihan028@gmail.com>
2026-05-18 00:43:50 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import LocationDetector from "@/components/LocationDetector";
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<p>Loading...</p>}>
       <LocationDetector />
-    </>
+    </Suspense>
   );
 }
